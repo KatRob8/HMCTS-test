97676974ed80d0936067bda6575eaeacf2d49fc3
Katie Roberts <kateroberts817@gmail.com>
2025-11-30 01:42:06 +0000

    Add main section and wrapper styles

--- client/src/components/MainContent.tsx
+++ client/src/components/MainContent.tsx
@@ -3,9 +3,12 @@ function MainContent() {
 
 
   return (
-    <main>
-        Task app 
-    </main>
+    <div className="govuk-width-container">
+        <main className="govuk-main-wrapper" id="main-content">
+            <h1 className="govuk-heading-xl">Tasks</h1>
+        </main>
+    </div>
+ 
   )
 }
 
