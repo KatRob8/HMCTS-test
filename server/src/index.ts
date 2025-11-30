cb68e22745285b0b63a1ac82e6766f7053a082f0
Katie Roberts <kateroberts817@gmail.com>
2025-11-30 23:42:43 +0000

    Connect to cloud pg database

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';  
 import cors from 'cors';
 import env from 'dotenv';
+import { Client } from 'pg';
 
 const app = express();
 env.config({ path: '../.env' });
@@ -11,18 +12,41 @@ const corsOptions = {
     credentials: true
 };
 
+// Create a new client instance
+const db = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, 
+    },
+});
+
+// Connect to database
+try {
+    await db.connect();
+    console.log('Connected to PostgreSQL');
+} catch (error) {
+    console.error('Error connecting to PostgreSQL:', error);
+}
+
+
 // Middleware 
 app.use(cors(corsOptions)); 
 app.use(express.json());
 
 // Routes
 
-app.post('/submit-task', (req: Request, res: Response) => {
-    const data = req.body;
-    console.log(data)
+app.post('/submit-task', async (req: Request, res: Response) => {
+    // Get data from form
+    const {title, description, status, dateTime} = req.body.task;
+
+    try {
+        await db.query("INSERT INTO tasks (title, description, status, datetime) VALUES ($1, $2, $3, $4)", [title, description, status, dateTime]);
+    } catch (error) {
+        console.log(error)
+    }
+    
     res.status(200).json({
         message: 'Data received successfully!',
-        data
     });
 });
 
