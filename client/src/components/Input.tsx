47f3722335bf70dc7aa1ff77045e8ab882f1ebe4
Katie Roberts <kateroberts817@gmail.com>
2025-12-07 02:57:59 +0000

    Fix footer CSS

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -11,11 +11,11 @@ type Props = {
 function Input({innerText, id, name, value, type, isRequired, updateData}: Props) {
 
     return (
-        <div className="govuk-!-margin-bottom-3">
+        <div className="govuk-form-group govuk-!-margin-bottom-3">
             <label className="govuk-label" htmlFor={id}>
                 {innerText}
             </label>
-            <input className="govuk-input" id={id} name={name} type={type} value={value} onChange={(e) => updateData(e)} required={isRequired} />
+            <input className="govuk-input govuk-!-width-full" id={id} name={name} type={type} value={value} onChange={(e) => updateData(e)} required={isRequired} />
         </div>      
     )
 }
